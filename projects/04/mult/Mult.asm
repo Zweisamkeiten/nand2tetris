fd3b2b4a03458fdd75835a575c22f5573176e91b
Einsam <snitxmhm@gmail.com>
2022-10-26 16:18:29 +0800

    p04: Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,40 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// for (i=R1; i > 0; i--) {
+//   R2 = R2 + R0;
+//   }
+
+    // initiallize R2 = 0
+    @R2
+    M=0
+
+    // initiallize i = value of R1
+    @R1
+    D=M
+    @i
+    M=D
+
+  (LOOP)
+    // if (i == 0) goto END
+    @i
+    D=M
+    @END
+    D;JEQ
+
+    // R2 = R2 + R0
+    @R0
+    D=M
+    @R2
+    M=D+M
+
+    // i--
+    @i
+    M=M-1
+
+    @LOOP
+    0;JMP
+
+  (END)
+    @END
+    0; JMP
